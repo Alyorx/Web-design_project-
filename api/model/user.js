b55b3aa14dd06df96f68966cae2c776df65e7602
YoussefBeshnack <youssefbeshnack@gmail.com>
2025-12-17 23:01:20 +0200

    Added Backend APIs for user

---
+++ api/model/user.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    _id: { type: String, required: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true, lowercase: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ['user', 'admin'], default: 'user' },
+    enrolledCourses: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Course' }],
+    lastActive: { type: String },
+  },
+  {
+    versionKey: false
+  }
+);
+
+export default mongoose.model('User', userSchema);
